53824f2b558ec8a6d7258fe01c8555b9ce73134b
Cord Hamrick <cordh@cordsworks.com>
2024-06-09 18:50:19 -0400

    ProductCard and AddToCart components;

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <h2>Product Name Here</h2>
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <h1>Hello, NextJS-01!</h1>
+      <ProductCard />
+      <Link href="/users">Users</Link>
     </main>
   );
 }
