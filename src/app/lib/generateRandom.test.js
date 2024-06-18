2dbd89c9561459fbd341efc7262ecb7d13d30071
Cord Hamrick <cordh@cordsworks.com>
2024-06-18 13:41:35 -0400

    Added describe-block;

--- src/app/lib/generateRandom.test.js
+++ src/app/lib/generateRandom.test.js
@@ -1,10 +1,12 @@
 import generateRandom from './generateRandom';
 
-test('random number output is between rangeStart and rangeEnd', () => {
-  const rangeStart = 4;
-  const rangeEnd = 22;
-  const generated = generateRandom(4, 22);
-
-  expect(generated).toBeGreaterThanOrEqual(rangeStart);
-  expect(generated).toBeLessThanOrEqual(rangeEnd);
+describe('Group tests of generateRandom (only 1 at present)', () => {
+  test('random number output is between rangeStart and rangeEnd', () => {
+    const rangeStart = 4;
+    const rangeEnd = 22;
+    const generated = generateRandom(4, 22);
+  
+    expect(generated).toBeGreaterThanOrEqual(rangeStart);
+    expect(generated).toBeLessThanOrEqual(rangeEnd);
+  });
 });
