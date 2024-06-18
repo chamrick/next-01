39e881e47cefe941ae662ac9092f76f14ae122bb
Cord Hamrick <cordh@cordsworks.com>
2024-06-18 13:57:22 -0400

    Mocking in Jest;

--- src/app/lib/generateRandom.test.js
+++ src/app/lib/generateRandom.test.js
@@ -1,11 +1,24 @@
 import generateRandom from './generateRandom';
 
+// jest.mock(
+//   '../path/to/some/module',
+//   () => ({
+//     functionA: jest.fn(() => {
+//       return 'contentA';
+//     }),
+//     functionB: jest.fn(() => {
+//       return 'contentB';
+//     }),
+//   })
+// );
+
+
 describe('Group tests of generateRandom (only 1 at present)', () => {
   test('random number output is between rangeStart and rangeEnd', () => {
     const rangeStart = 4;
     const rangeEnd = 22;
     const generated = generateRandom(4, 22);
-  
+
     expect(generated).toBeGreaterThanOrEqual(rangeStart);
     expect(generated).toBeLessThanOrEqual(rangeEnd);
   });
