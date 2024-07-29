3670a6127d01c6f9cce6a9f71d0b8d4ad1bbcfac
Cord Hamrick <cordh@cordsworks.com>
2024-07-28 22:06:15 -0400

    Minor usage of .env var;

--- src/app/components/SiteLogo/SiteLogo.tsx
+++ src/app/components/SiteLogo/SiteLogo.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './SiteLogo.module.css';
 
+const siteTitle: String = process.env.SITE_TITLE || 'Sitename With Logo';
+
 const SiteLogo = () => {
   return (
     <div className={styles.sitenameWithLogo}>
       <Image className={styles.logoImage} src="/website-logo.png" width="50" height="50" alt="Logo" />
-      <label className={styles.siteName}>Sitename With Logo</label>
+      <label className={styles.siteName}>{siteTitle}</label>
     </div>
   );
 };
